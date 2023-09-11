6d8b95358ca51611f43d41c4c349e7bb5ddd435f
ricardo soto <ricardosr4@gmail.com>
2023-09-11 20:08:25 -0300

    ok

--- app/src/main/java/com/example/pruebaapi/data/source/DogsDataSource.kt
+++ app/src/main/java/com/example/pruebaapi/data/source/DogsDataSource.kt
@@ -2,9 +2,10 @@ package com.example.pruebaapi.data.source
 
 import com.example.pruebaapi.data.remote.model.DogsResponse
 import com.example.pruebaapi.data.remote.network.APIService
+import retrofit2.Response
 
 class DogsDataSource(private val apiService: APIService) {
-    suspend fun getDogs(user_name:String): List<DogsResponse>{
+    suspend fun getDogs(user_name:String): Response<DogsResponse>{
         return apiService.getDogsByBreeds(user_name)
     }
 
--- app/src/main/java/com/example/pruebaapi/presentation/viewModel/DogViewModel.kt
+++ app/src/main/java/com/example/pruebaapi/presentation/viewModel/DogViewModel.kt
@@ -6,17 +6,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.pruebaapi.data.remote.model.DogsResponse
+import com.example.pruebaapi.data.repository.DogsRepository
+import com.example.pruebaapi.domain.useCase.DogsUseCase
 import kotlinx.coroutines.launch
 
 
 class DogViewModel : ViewModel() {
     var dogsModel = MutableLiveData<List<DogsResponse>>()
     val isloading = MutableLiveData<Boolean>()
+    val DogsUseCase = MutableLiveData<List<DogsRepository>>()
 
-    fun getMatches(user_name: String) {
+    fun getDogs(user_name: String) {
         viewModelScope.launch {
             isloading.postValue(true)
-            val result = getMatchesRemoteUseCase.getMatchesFire(user_name)
+            val result = DogsUseCase.getDogs(user_name)
             if (!result.isNullOrEmpty()) {
                 dogsModel.postValue(result)
                 isloading.postValue(false)
